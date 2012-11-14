bc459c2d05fd25366a0a22af8c9dec421ceabadc
j-santander <jsantander2@gmail.com>
2012-11-14 09:32:52 +0100

    Enhance SDP parser to further decode the attribute lines.

--- sdp.js
+++ sdp.js
@@ -1,3 +1,9 @@
+
+/****************************
+ * 
+ * SDP Parsing functions
+ * 
+ ****************************/
 //
 // Functions made available
 //
@@ -6,7 +12,12 @@ exports.parse=parse;
 exports.stringify=stringify;
 
 
+// Regular expression to parse media lines
 var reM = /^(\w+) +(\d+)(?:\/(\d))? +(\S+) (\d+( +\d+)*)/;
+// Regular expression to parse attribute lines.
+var reA = /^([\w-]+)(?::(.*))?$/;
+
+// Specialized parsers for each line
 var parsers = {
 	//v=0
 	//o=+34913305131 0 0 IN IP4 127.0.0.1 
@@ -53,14 +64,91 @@ var parsers = {
 	//a=rtpmap:9 G722/8000
 	//a=recvonly
 	a : function(r, value) {
-		if (!r.a) {
-			r.a = new Array();
+		var t = reA.exec(value);
+		if(!r.a) {
+			r.a={};
 		}
-		r.a.push(value);
+		// For attributes 
+		// we're going to store them as key-values under 'a' key.
+		// the exception will be the codec-related attributes that
+		// will go under a separate key (fmta) keyed by the codec number
+		// attributes key-only (e.g. 'recvonly') will have null value.		
+		if(t) {
+			var key=t[1];
+			var value=null;
+			if(t[2]) {
+				value=t[2];
+			}
+			if (aParsers[key]) {
+				aParsers[key](r, value);
+			} else {
+				r.a[key]=value
+			}
+		} else {
+			// Couldn't parse the attribute
+			// add it as an array under "a"
+			if (!r.a.other) {
+				r.a.other = new Array();
+			}			
+			r.a.other.push(value);	
+		}
 		return value;
 	}
 };
 
+//
+// Specialized parsers for attribute lines
+//
+var aParsers={
+	rtcp: function(r,value) {
+		//port IN IP4 192.168.1.37
+		var t = value.split(/\s+/);
+		r.a.rtcp = {
+				port : t[0],
+				nettype : t[1],
+				addrtype : t[2],
+				address : t[3]
+		};
+		return r.a.rtcp;
+	},
+	rtpmap: function(r,value) {
+		// codec options
+		return parseCodecAttribute("rtpmap",r,value);
+	},
+	fmtp: function(r,value) {
+		//codec options
+		return parseCodecAttribute("fmtp",r,value);
+	}	
+}
+
+/**
+ * Function to add an attribute to a particular codec.
+ * @param attribute the attribute name
+ * @param r the media object
+ * @param value the attribute value
+ * @returns the media object
+ */
+function parseCodecAttribute(attribute,r,value) {
+	var t = /^(\d+) ?(.*)/.exec(value);
+	if(t) {
+		var codec=t[1];
+		var value=t[2];
+		if(!r.fmta) {
+			r.fmta={};
+		} 
+		if(!r.fmta[codec]) {
+			r.fmta[codec]={}
+		}
+		r.fmta[codec][attribute]=value;
+		return r.fmta[codec][attribute];
+	} 
+	return r;
+	
+}
+
+
+// Contains specialized stringifiers for the
+// different SDP lines.
 var stringifiers = {
 	o : function(o) {
 		return [ o.username || '-', o.id, o.version, o.nettype || 'IN',
@@ -72,8 +160,25 @@ var stringifiers = {
 	m : function(m) {
 		return [ m.media || 'audio', m.port, m.transport || 'RTP/AVP',
 				m.fmt.join(' ') ].join(' ');
-	}
+	},
 };
+
+// Contains specialized stringifiers for
+// attribute values.
+var aStringifiers = {
+	rtcp: function(a) {
+		return ':'+([ a.port, a.nettype || 'IN', a.addrtype || 'IP4', a.address ].join(' '));
+	},
+	rtpmap: function(a) {
+		return ':'+([ a.codec, a.value].join(' '));
+	},
+	fmtp: function(a) {
+		return ':'+([ a.codec, a.value].join(' '));
+	},
+	other: function(a) {
+		return a;
+	}
+}
 
 /**
  * Parse an SDP content
@@ -99,6 +204,13 @@ function parse(sdp) {
 	return result;
 }
 
+/**
+ * Parse a Line
+ * @param r base object to add to 
+ * @param key type of line
+ * @param value value of the line
+ * @returns the resulting object
+ */
 function parseLine(r, key, value) {
 	if (parsers[key]) {
 		return parsers[key](r, value);
@@ -107,6 +219,12 @@ function parseLine(r, key, value) {
 	}
 }
 
+/**
+ * Provides a string version of
+ * the SDP structure
+ * @param sdp the SDP to map
+ * @returns the result
+ */
 function stringify(sdp) {
 	var s = '';
 	s += stringifyLine(sdp, 'v', 0);
@@ -122,7 +240,7 @@ function stringify(sdp) {
 	s += stringifyLine(sdp, 'r');
 	s += stringifyLine(sdp, 'z');
 	s += stringifyLine(sdp, 'k');
-	s += stringifyLine(sdp, 'a');
+	s += stringifyAttributes(sdp);
 	sdp.m.forEach(function(m) {
 		s += stringifyLine({
 			m : m
@@ -131,12 +249,21 @@ function stringify(sdp) {
 		s += stringifyLine(m, 'c');
 		s += stringifyLine(m, 'b');
 		s += stringifyLine(m, 'k');
-		s += stringifyLine(m, 'a');
+		s += stringifyCodecAttributes(m);
+		s += stringifyAttributes(m);
 	});
 
 	return s;
 }
 
+/**
+ * Process a line type to produce
+ * its line version
+ * @param sdp the structure
+ * @param type the type of line
+ * @param def the default value if not present
+ * @returns the string representation
+ */
 function stringifyLine(sdp, type, def) {
 	if (sdp[type] != undefined) {
 		var stringifier = function(x) {
@@ -154,4 +281,68 @@ function stringifyLine(sdp, type, def) {
 		return type + '=' + def + '\r\n';
 	}
 	return '';
+}
+
+/**
+ * Process all the attribute key-values 
+ * @param sdp the sdp structure containing the attributes
+ * @returns the string representation.
+ */
+function stringifyAttributes(sdp) {
+	if(!sdp.a) {
+		return '';
+	}
+	
+	//
+	// Now, go over all keys, mapping each item
+	return Object.keys(sdp.a).map(function(k) {
+		// Mapping function, transform each elemt
+		var stringifier = function(x) {
+			// Stringifier function: get the string representation.
+			var result='a=';
+			if(aStringifiers[k]) {
+				// Found a function to process this
+				result+=k+aStringifiers[k](x);
+			} else {
+				// No function, check if it just a presence key
+				if(x==null) {
+					result+=k;
+				} else {
+					// Then it is a name value
+					result+=k+":"+x;
+				}
+			}
+			return result+'\r\n';
+		}
+		// If the key contains an array, map all of them
+		if (Array.isArray(sdp.a[k])) {
+			return sdp.a[k].map(stringifier).join('');
+		}
+		// Otherwise just call the function.
+		return stringifier(sdp.a[k]);		
+	}).join('');
+}
+
+/**
+ * Handle the special case of the codec attributes
+ * @param m the sdp structure (media)
+ * @return the string representation.
+ */
+function stringifyCodecAttributes(m) {
+	if(!m.fmta) {
+		return '';
+	}
+	// We're going to create a fake 'a' object.
+	// where we will add the attributes and then
+	// process as if they were regular a= lines.
+	return Object.keys(m.fmta).map(function(codec) {
+		var a={};
+		Object.keys(m.fmta[codec]).forEach(function (k) {
+			a[k]={
+					codec: codec,
+					value: m.fmta[codec][k]
+			};
+		});
+		return stringifyAttributes({a:a});
+	}).join('');
 }
